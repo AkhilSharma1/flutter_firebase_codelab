a1902e4ce98545c1af25843d0685bd4fd22218fc
akhil <akhil.sharma0910@gmail.com>
2018-01-05 10:44:48 -0500

    add skeleton project

---
+++ codelab_my_friendlychat/README.md
@@ -0,0 +1,3 @@
+# Flutter examples
+
+FriendlyChat codelab project for &#x27;Building Beautiful UIs&#x27;
---
+++ codelab_my_friendlychat/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new FriendlyChat());
+
+class FriendlyChat extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Friendly Chat',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Friendly Chat')),
+    );
+  }
+}
