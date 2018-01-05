f8a4f4138daee96f8e632332e2f20c5c0c8b2aa1
akhil <akhil.sharma0910@gmail.com>
2018-01-05 13:55:33 -0500

    add top-level layout structure

--- codelab_my_friendlychat/lib/main.dart
+++ codelab_my_friendlychat/lib/main.dart
@@ -16,11 +16,36 @@ class FriendlyChat extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text('Friendly Chat')),
+      body: new Column(
+        children: <Widget>[
+          new Expanded(child: new Center(child: new Text('Show List Here'))),
+          new Divider(
+            height: 1.0,
+          ),
+          new Container(color: Colors.blue, child: _buildTextComposer())
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return new Row(
+        children: <Widget>[
+          new Text('Text composer')]
     );
   }
 }
