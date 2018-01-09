aba565fdba1e5166328f637d257e91e6e412c874
akhil <akhil.sharma0910@gmail.com>
2018-01-09 16:17:24 -0500

    choose image from image picker and upload it to firebase cloud.

--- friendlychat_firebase/lib/main.dart
+++ friendlychat_firebase/lib/main.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
+import 'dart:io';
+import 'dart:math';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:image_picker/image_picker.dart';
+
 
 final googleSignIn = new GoogleSignIn();
 final analytics = new FirebaseAnalytics();
@@ -93,7 +98,15 @@ class ChatScreenState extends State<ChatScreen> {
           new Container(
             margin: new EdgeInsets.symmetric(horizontal: 4.0),
             child: new IconButton(
-                icon: new Icon(Icons.photo_camera), onPressed: () {}),
+                icon: new Icon(Icons.photo_camera), onPressed: () async{
+                  await _ensureLoggedIn();
+                  File imageFile = await ImagePicker.pickImage();
+                  int random = new Random().nextInt(100000);
+                  StorageReference ref =
+                  FirebaseStorage.instance.ref().child("image_$random.jpg");
+                  StorageUploadTask uploadTask = ref.put(imageFile);
+                  Uri downloadUrl = (await uploadTask.future).downloadUrl;
+            }),
           ),
           new Flexible(
               child: new TextField(
